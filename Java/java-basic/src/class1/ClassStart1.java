b1e40698e8848fb756423a1ff90804742c2715ad
Kobe <dev.skyachieve91@gmail.com>
2024-02-15 15:21:40 +0900

    :sparkles: Feat: '클래스가 필요한 이유' 학습.

---
+++ Java/java-basic/src/class1/ClassStart1.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class ClassStart1 {
+
+  public static void main(String[] args) {
+    String firstStudentName = "학생1";
+    String secondStudentName = "학생2";
+
+    int firstStudentAge = 15;
+    int secondStudentAge = 16;
+
+    int firstStudentGrade = 90;
+    int secondStudentGrade = 80;
+
+    System.out.println("이름: " + firstStudentName + " 나이: " + firstStudentAge + " 성적: " + firstStudentGrade);
+    System.out.println("이름: " + secondStudentName + " 나이: " + secondStudentAge + " 성적: " + secondStudentGrade);
+  }
+
+}
