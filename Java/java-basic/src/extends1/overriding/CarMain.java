5fe5e6a69424e6574c59aae77f384fd515917743
Kobe <dev.skyachieve91@gmail.com>
2024-03-09 17:21:32 +0900

    :sparkles: Feat: '상속과 메서드 오버라이딩' 학습.

--- Java/java-basic/src/extends1/overriding/CarMain.java
+++ Java/java-basic/src/extends1/overriding/CarMain.java
@@ -1,16 +1,12 @@
-package extends1.ex1;
+package extends1.overriding;
 
 public class CarMain {
 
   public static void main(String[] args) {
-
-
     ElectricCar electricCar = new ElectricCar();
     electricCar.move();
-    electricCar.charge();
 
     GasCar gasCar = new GasCar();
     gasCar.move();
-    gasCar.fillUp();
   }
 }
